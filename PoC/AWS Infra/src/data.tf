fc8de53f384d5f9fd39f04c285252e1f6a035cca
jjjjjunhyung <56262868+jjjjjunhyung@users.noreply.github.com>
2025-01-17 17:35:03 +0900

    Update data.tf

--- PoC/AWS Infra/src/data.tf
+++ PoC/AWS Infra/src/data.tf
@@ -1,1 +1,10 @@
 data "aws_caller_identity" "current" {}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.6*-kernel-*-x86_64"]
+  }
+}
