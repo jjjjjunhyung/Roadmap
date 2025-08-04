d85d33c93e148f00ab963e16f542286511ebdbda
jjjjjunhyung <56262868+jjjjjunhyung@users.noreply.github.com>
2025-08-04 16:59:53 +0900

    Update provider.tf

---
+++ Toy Project/Real-Time Chat Service/infra/provider.tf
@@ -0,0 +1,8 @@
+# Configure the Oracle Cloud Infrastructure Provider
+provider "oci" {
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
+  region           = var.region
+}
