c785b1ceea978d4c3f6912a9009455cd7289a5f0
jjjjjunhyung <56262868+jjjjjunhyung@users.noreply.github.com>
2025-08-04 17:00:46 +0900

    Create versions.tf

---
+++ Toy Project/Real-Time Chat Service/infra/versions.tf
@@ -0,0 +1,10 @@
+# Oracle Cloud Infrastructure Provider Configuration
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = "~> 7.0"
+    }
+  }
+}
